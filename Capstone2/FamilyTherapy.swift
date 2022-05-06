a66970aa2fcf7b8108fdb4872c8760f2445e12ad
Jeffrey Voltaire <Jeffreyv300@gmail.com>
2022-05-06 09:23:35 -0400

    videos

---
+++ Capstone2/FamilyTherapy.swift
@@ -0,0 +1,41 @@
+//
+//  FamilyTherapy.swift
+//  Capstone2
+//
+//  Created by Jeffrey Voltaire on 5/5/22.
+//
+
+import AVKit
+import AVFoundation
+import UIKit
+
+class FamilyTherapy: UIViewController {
+
+    @IBOutlet weak var watchVideo: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func videoTapped(_ sender: Any) {
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "FamilyTherapy", ofType: "mp4")!))
+                    let vc = AVPlayerViewController()
+                    vc.player = player
+                    present(vc, animated: true)
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
